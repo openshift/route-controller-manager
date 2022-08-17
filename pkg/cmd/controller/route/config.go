ravisantoshgudimetla/openshift-controller-manager

Split route controller

pkg/cmd/controller/route-controller/config.go
@@ -1,0 +1,13 @@
+package route
+
+var ControllerInitializers = map[string]InitFunc{
+	"openshift.io/ingress-ip":       RunIngressIPController,
+	"openshift.io/ingress-to-route": RunIngressToRouteController,
+}
+
+const (
+	infraServiceIngressIPControllerServiceAccountName = "service-ingress-ip-controller"
+	infraIngressToRouteControllerServiceAccountName   = "ingress-to-route-controller"
+
+	defaultOpenShiftInfraNamespace = "openshift-infra"
+)

openshift/route-controller-manager

pkg/cmd/controller/route/config.go
@@ -1,0 +1,13 @@
+package route
+
+var ControllerManagerInitialization = map[string]InitFunc{
+	"openshift.io/ingress-ip":       RunIngressIPController,
+	"openshift.io/ingress-to-route": RunIngressToRouteController,
+}
+
+const (
+	infraServiceIngressIPControllerServiceAccountName = "service-ingress-ip-controller"
+	infraIngressToRouteControllerServiceAccountName   = "ingress-to-route-controller"
+
+	defaultOpenShiftInfraNamespace = "openshift-infra"
+)