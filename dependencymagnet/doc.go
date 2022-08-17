openshift/openshift-controller-manager

add dependency magnet for go mod

make/doc.go
@@ -1,0 +1,7 @@
+package make
+
+import (
+	// alpha-build-machinery has non-go code needed for makefiles.
+	// Needed to work with go modules.
+	_ "github.com/openshift/library-go/alpha-build-machinery"
+)

openshift/route-controller-manager

dependencymagnet/doc.go
@@ -1,0 +1,7 @@
+package dependencymagnet
+
+import (
+	// alpha-build-machinery has non-go code needed for makefiles.
+	// Needed to work with go modules.
+	_ "github.com/openshift/library-go/alpha-build-machinery"
+)